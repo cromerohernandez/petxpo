cbdd91065e2f9bfe14b9692baec7cbc79c57406c
cromerohernandez <c.romero.hsv@gmail.com>
2021-03-20 11:45:03 +0100

    DEV: like

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -1,13 +1,38 @@
+import { useContext, useState, useCallback, useEffect } from 'react'
+
+import AuthContext from '../contexts/AuthContext'
+
 import FirebaseService from '../services/FirebaseService'
 
-const LikeButton = ({ id, likes }) => {
+const LikeButton = ({ id, getLikes }) => {
+  const auth = useContext(AuthContext)
+
+  const [userLike, setUserLike] = useState()
+
+  const getUserLike = useCallback(() => {
+    FirebaseService.getUserLike(auth.currentUser, id)
+      .then(userLike => {
+        setUserLike(userLike)
+      })
+      //.catch
+  }, [userLike])
+
+  useEffect(() => {
+    getUserLike()
+  }, [])
+
   const handleLike = () => {
-    FirebaseService.like(id)
+    FirebaseService.like(userLike.id, auth.currentUser, id)
+    getLikes()
+    getUserLike()
   }
   
   return (
     <div>
-      <button onClick={handleLike}>{likes}</button>
+      <button onClick={handleLike}>Like</button>
+      {userLike && (
+        <h6>Yes!</h6>
+      )}
     </div>
   )
 }
--- src/components/PetCard.js
+++ src/components/PetCard.js
@@ -1,11 +1,32 @@
+import { useState, useEffect, useCallback } from 'react'
+
+import FirebaseService from '../services/FirebaseService'
+
 import LikeButton from './LikeButton'
 
 const PetCard = ({ pet }) => {
+  const [likes, setLikes] = useState(0)
+
+  const getLikes = useCallback(() => {
+    FirebaseService.getLikes(pet.id)
+    .then(petLikes => {
+      setLikes(petLikes)
+    })
+    //.catch
+  }, [likes])
+
+  useEffect(() => {
+    getLikes()
+  }, [])
+
   return (
     <div>
     <h2>{pet.name}</h2>
     <img src={pet.photo} alt={`${pet.name}jpg`} />
-    <LikeButton id={pet.id} likes={pet.likes} />
+    <div>
+      <h6>{likes}</h6>
+      <LikeButton id={pet.id} getLikes={getLikes} />
+    </div>
   </div>
   )
 }
